6d5cecfd4d5d9ff0a9d6977c182db17d990bbc31
akila <akilaocj@gmail.com>
2023-08-02 12:23:22 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,114 @@
+public class Main {
+    public static void main(String[] args) {
+//        // statement
+//        int sum1 = 3 + 4 * 2;
+//        System.out.println(sum1);
+//
+//        // statement
+//        String greeting = "Hello, world!";
+//
+//        // statement
+//        if (sum1 > 4) {
+//            System.out.println(greeting);
+//        }
+//
+//
+//
+//        // Expressions
+//        // Arithmetic expression
+//        int result = 10 * (5 - 3);
+//
+//        // String expression
+//        String completeGreeting = greeting + " How are you?";
+//
+//        // Boolean expression
+//        boolean isResultGreaterThanTen = result > 10; //true or false
+//
+//
+//
+//
+//        // Conditional Statements: If-Else and Switch
+//        int temperature = 20;
+//
+//        if (temperature > 30) { // if (true)
+//            System.out.println("It's hot outside!");
+//        } else { // if (false)
+//            System.out.println("It's not hot outside.");
+//        }
+
+
+//        char grade = 'Y';
+//        switch (grade) {
+//            case 'A':
+//                System.out.println("Excellent!");
+//                break;
+//            case 'B':
+//                System.out.println("Good!");
+//                break;
+//            case 'C':
+//                System.out.println("Good2!");
+//                break;
+//            default:
+//                System.out.println("Keep trying!");
+//        }
+
+
+
+//        boolean isEarlyMorning = false;
+//        boolean isCoffeeAvailable = false;
+//
+////        ---- switch ---- switch ----
+////        |                           |
+////        |________X --------------|
+//
+//        if (isEarlyMorning && isCoffeeAvailable) {
+//            System.out.println("Need to buy coffee!");
+//        }else {
+//            System.out.println("I am false");
+//        }
+
+
+//        int score = 65;
+//
+//        if (score > 90) {
+//            System.out.println("You did great!");
+//        } else if (score > 80) {
+//            System.out.println("You did good!");
+//        } else if (score > 70) {
+//            System.out.println("You did good2!");
+//        }
+//        else if (score > 60) {
+//            System.out.println("You did good3!");
+//        }else {
+//            System.out.println("You need to study more.");
+//        }
+
+//        int score = 55;
+//
+//
+//        if (score > 60) {
+//            if (score > 70) {
+//                System.out.println("You passed the exam.");
+//            } else {
+//                System.out.println("You barely passed the exam.");
+//            }
+//        } else {
+//            System.out.println("You failed the exam.");
+//        }
+
+
+//        boolean isPasswordCorrect = false;
+//
+//        String password = "pass123";
+//        if (password.equals("pass1234")) {
+//            isPasswordCorrect = true;
+//        }
+//
+//        if (isPasswordCorrect) {
+//            System.out.println("Access granted!");
+//        } else {
+//            System.out.println("Access denied!");
+//        }
+
+    }
+}
